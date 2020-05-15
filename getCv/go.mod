032fcb9232cfed9d8b8a56abc1ed2b9329b88b13
SegFault42 <SegFault42@protonmail.com>
2020-05-16 01:40:08 +0300

    clone repo

---
+++ getCv/go.mod
@@ -0,0 +1,15 @@
+module getCV
+
+go 1.14
+
+require (
+	github.com/astaxie/beego v1.12.1
+	github.com/go-git/go-git v4.7.0+incompatible
+	github.com/go-git/go-git/v5 v5.0.0
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	github.com/sergi/go-diff v1.1.0 // indirect
+	github.com/shiena/ansicolor v0.0.0-20151119151921-a422bbe96644 // indirect
+	golang.org/x/crypto v0.0.0-20200510223506-06a226fb4e37 // indirect
+	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
+	gopkg.in/src-d/go-git.v4 v4.13.1
+)
--- getCv/main.go
+++ getCv/main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
+	"os/user"
 	"strconv"
 	"strings"
+
+	"github.com/astaxie/beego/logs"
+	"github.com/go-git/go-git/v5"
+	"gopkg.in/src-d/go-git.v4/plumbing/transport/ssh"
 )
 
 type sToken struct {
@@ -19,6 +24,7 @@ type sToken struct {
 }
 
 type intraProject struct {
+	Login         string `json:login`
 	ProjectsUsers []struct {
 		ID        int  `json:id`
 		Validated bool `json:"validated?"`
@@ -26,6 +32,12 @@ type intraProject struct {
 			ID int `json:"id"`
 		} `json:"project"`
 	} `json:"projects_users"`
+}
+
+type vogsphereRepo struct {
+	Teams []struct {
+		RepoURL string `json:"repo_url"`
+	} `json:"teams"`
 }
 
 func getNewToken(uid, secret string) string {
@@ -68,7 +80,7 @@ func bearerGetRequest(token, url string) *http.Response {
 	return resp
 }
 
-func getProjectID(intraUid int, token string) int {
+func getProjectID(intraUid int, token string) (int, string) {
 	url := "https://api.intra.42.fr/v2/users/" + strconv.Itoa(intraUid)
 	resp := bearerGetRequest(token, url)
 
@@ -85,11 +97,61 @@ func getProjectID(intraUid int, token string) int {
 	// check if project 902 is validate
 	for i := 0; i < len(project.ProjectsUsers); i++ {
 		if project.ProjectsUsers[i].Project.ID == 902 && project.ProjectsUsers[i].Validated == true {
-			return project.ProjectsUsers[i].ID
+			return project.ProjectsUsers[i].ID, project.Login
 		}
 	}
 
-	return 0
+	return 0, ""
+}
+
+func getVogsphereAddress(projectId int, token string) string {
+	url := "https://api.intra.42.fr/v2/projects_users/" + strconv.Itoa(projectId)
+	resp := bearerGetRequest(token, url)
+
+	body, _ := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+
+	var vogsphere vogsphereRepo
+
+	err := json.Unmarshal([]byte(body), &vogsphere)
+	if err != nil {
+		panic(err)
+	}
+
+	return vogsphere.Teams[0].RepoURL
+}
+
+func cloneRepo(vogsphereRepo, login string) {
+	dir := "./" + login
+	os.Mkdir(dir, os.ModePerm)
+
+	// clone with ssh key
+	currentUser, err := user.Current()
+	if err != nil {
+		logs.Error(err)
+	}
+	sshAuth, err := ssh.NewPublicKeysFromFile("git", currentUser.HomeDir+"/.ssh/id_rsa", "admin")
+	if err != nil {
+		logs.Error(err)
+	}
+	// Clones the repository into the given dir, just as a normal git clone does
+	_, err = git.PlainClone(dir, false, &git.CloneOptions{
+		URL:      vogsphereRepo,
+		Auth:     sshAuth,
+		Progress: os.Stdout,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	os.RemoveAll(login + "/.git")
+
+	// Prints the content of the CHANGELOG file from the cloned repository
+	// changelog, err := os.Open(filepath.Join(dir, ".git/config"))
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// io.Copy(os.Stdout, changelog)
 }
 
 func main() {
@@ -97,7 +159,17 @@ func main() {
 	clientSecret := os.Getenv("CLIENTSECRET")
 
 	token := getNewToken(clientId, clientSecret)
+	log.Println("Get token OK")
 
-	projectId := getProjectID(61663, token)
-	fmt.Println(projectId)
+	projectId, login := getProjectID(61663, token)
+	log.Println("GetProjectId ok")
+
+	if projectId != 0 {
+		vogsphereAddress := getVogsphereAddress(projectId, token)
+		log.Println("Get Vogsphere address ok")
+		if vogsphereAddress != "" {
+			log.Println("Cloning Repo ...")
+			cloneRepo(vogsphereAddress, login)
+		}
+	}
 }
